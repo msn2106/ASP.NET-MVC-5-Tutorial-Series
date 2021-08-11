c038a8dc6f08682d6af06957d5548db72e624f1a
msn2106 <msn2106@gmail.com>
2021-08-11 17:32:14 +0530

    Added IdentityFrameworkInternalDB

---
+++ IdentityFrameworkInternalDB/IdentityFrameworkInternalDB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityFrameworkInternalDB.Startup))]
+namespace IdentityFrameworkInternalDB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
