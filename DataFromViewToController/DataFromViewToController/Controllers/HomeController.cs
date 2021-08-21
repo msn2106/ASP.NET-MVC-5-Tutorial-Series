73e7012d73c89c37351cd2761b6c0f784f9c3a53
msn2106 <msn2106@gmail.com>
2021-08-21 13:55:37 +0530

    Pass data from View to Controller

---
+++ DataFromViewToController/DataFromViewToController/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataFromViewToController.Models;
+
+namespace DataFromViewToController.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string PostUsingParameters(string firstName, string lastName)
+        {
+            return "From Parameters:"+ firstName + " " + lastName;
+        }
+
+        [HttpPost]
+        public string PostUsingRequests()
+        {
+            string firstName = Request["firstName"];
+            string lastName = Request["lastName"];
+
+            return "From Requests:" + firstName + " " + lastName;
+        }
+
+        //[HttpPost]
+        public string PostUsingFormCollection(FormCollection form)
+        {
+            string firstName = form["firstName"];
+            string lastName = form["lastName"];
+
+            return "From Form Collection :" + firstName + " " + lastName;
+        }
+
+        public string PostUsingBinding(Employee emp)
+        {
+            return "From Strongly Binding :" + emp.firstName + " " + emp.lastName;
+        }
+    }
+}
